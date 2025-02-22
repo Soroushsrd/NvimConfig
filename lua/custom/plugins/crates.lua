c5518382468092972b67e974df1574510e6e372b
Soroushsrd <Sardashtisoroosh@gmail.com>
2025-02-22 17:01:59 +0330

    Added Transparent plugin

--- lua/custom/plugins/crates.lua
+++ lua/custom/plugins/crates.lua
@@ -3,6 +3,10 @@ return {
   ft = { 'toml' },
   config = function()
     require('crates').setup {
+      curl_args = { '-sL', '--retry', '3', '--retry-delay', '1', '--connect-timeout', '5' },
+      max_parallel_requests = 20, -- Lower this to reduce connection load
+      loading_indicator = true,
+      autoupdate_throttle = 1000, -- Increase throttle to reduce network requests
       completion = {
         cmp = {
           enabled = true,
