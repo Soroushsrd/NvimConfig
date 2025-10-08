5c139783f420f3e85385cc33b3344ddff10cf92a
Soroushsrd <Sardashtisoroosh@gmail.com>
2025-10-08 12:13:01 +0330

    theme changes

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -14,7 +14,7 @@
 -- return {
 --   'rebelot/kanagawa.nvim',
 --   config = function()
---     vim.cmd 'colorscheme kanagawa-wave'
+--     vim.cmd 'colorscheme kanagawa-lotus'
 --   end,
 -- }
 -- return {
