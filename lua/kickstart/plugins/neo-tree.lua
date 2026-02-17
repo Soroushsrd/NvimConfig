80cca1b91c924ba79d2496f401655692544e1c95
Soroushsrd <Sardashtisoroosh@gmail.com>
2026-02-17 11:00:07 +0330

    Matched neo tree keybindings with my emacs

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { "<leader>'", ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ["<leader>'"] = 'close_window',
         },
         filtered_items = {
           visible = true,
